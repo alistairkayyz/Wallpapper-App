c47e4d6c07d87687bdfcd429ac1b5b44aa16bc8e
AListair Khosa <AListairKhosa@gmail.com>
2021-03-15 10:49:01 +0200

    first commit

---
+++ Wallpaper App/Form1.Designer.vb
@@ -0,0 +1,201 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmWallpaper
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.cboLength = New System.Windows.Forms.ComboBox()
+        Me.cboWidth = New System.Windows.Forms.ComboBox()
+        Me.cboHeight = New System.Windows.Forms.ComboBox()
+        Me.cboRollCov = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.lstDisplay = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'cboLength
+        '
+        Me.cboLength.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cboLength.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.cboLength.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cboLength.FormattingEnabled = True
+        Me.cboLength.Location = New System.Drawing.Point(184, 51)
+        Me.cboLength.Name = "cboLength"
+        Me.cboLength.Size = New System.Drawing.Size(121, 23)
+        Me.cboLength.TabIndex = 0
+        '
+        'cboWidth
+        '
+        Me.cboWidth.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cboWidth.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.cboWidth.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cboWidth.FormattingEnabled = True
+        Me.cboWidth.Location = New System.Drawing.Point(184, 116)
+        Me.cboWidth.Name = "cboWidth"
+        Me.cboWidth.Size = New System.Drawing.Size(121, 23)
+        Me.cboWidth.TabIndex = 1
+        '
+        'cboHeight
+        '
+        Me.cboHeight.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cboHeight.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.cboHeight.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cboHeight.FormattingEnabled = True
+        Me.cboHeight.Location = New System.Drawing.Point(184, 182)
+        Me.cboHeight.Name = "cboHeight"
+        Me.cboHeight.Size = New System.Drawing.Size(121, 23)
+        Me.cboHeight.TabIndex = 2
+        '
+        'cboRollCov
+        '
+        Me.cboRollCov.BackColor = System.Drawing.SystemColors.Window
+        Me.cboRollCov.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cboRollCov.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.cboRollCov.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cboRollCov.FormattingEnabled = True
+        Me.cboRollCov.Location = New System.Drawing.Point(184, 239)
+        Me.cboRollCov.Name = "cboRollCov"
+        Me.cboRollCov.Size = New System.Drawing.Size(121, 23)
+        Me.cboRollCov.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(33, 52)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(98, 16)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Length (feet):"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(33, 117)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(85, 15)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Width (feet):"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(33, 183)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(91, 15)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Height (feet):"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(33, 240)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(145, 15)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Roll coverage (sqrFt):"
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalculate.Location = New System.Drawing.Point(333, 178)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(93, 23)
+        Me.btnCalculate.TabIndex = 9
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(432, 178)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(93, 23)
+        Me.btnExit.TabIndex = 10
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(330, 109)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(77, 15)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Single roll:"
+        '
+        'lstDisplay
+        '
+        Me.lstDisplay.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstDisplay.FormattingEnabled = True
+        Me.lstDisplay.ItemHeight = 15
+        Me.lstDisplay.Location = New System.Drawing.Point(333, 127)
+        Me.lstDisplay.Name = "lstDisplay"
+        Me.lstDisplay.Size = New System.Drawing.Size(192, 34)
+        Me.lstDisplay.TabIndex = 12
+        '
+        'frmWallpaper
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Gainsboro
+        Me.ClientSize = New System.Drawing.Size(583, 314)
+        Me.Controls.Add(Me.lstDisplay)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.cboRollCov)
+        Me.Controls.Add(Me.cboHeight)
+        Me.Controls.Add(Me.cboWidth)
+        Me.Controls.Add(Me.cboLength)
+        Me.Name = "frmWallpaper"
+        Me.Text = "Wallpaper App"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents cboLength As System.Windows.Forms.ComboBox
+    Friend WithEvents cboWidth As System.Windows.Forms.ComboBox
+    Friend WithEvents cboHeight As System.Windows.Forms.ComboBox
+    Friend WithEvents cboRollCov As System.Windows.Forms.ComboBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents btnCalculate As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents lstDisplay As System.Windows.Forms.ListBox
+
+End Class
---
+++ Wallpaper App/Form1.vb
@@ -0,0 +1,73 @@
+﻿Public Class frmWallpaper
+
+    'Declaring variables
+    Dim dblRoll As Double
+    Dim intArea As Integer
+
+    'declaration and initializing arrayA for length, width and height
+    Dim arrayA As Integer() = New Integer() {10, 11, 12, 13, 14, 15, 16, 17, 18,
+                                             19, 20, 21, 22, 23, 24, 25, 26, 27,
+                                             28, 29, 30, 31, 32, 33, 34, 35}
+
+    'declaration and initializing arrayB for roll coverage
+    Dim arrayB As Double() = New Double() {40, 40.5, 41, 41.5, 42, 42.5, 43,
+                                           43.5, 44, 44.5, 45, 45.5, 46, 46.5,
+                                           47, 47.5, 48, 48.5, 49, 49.5, 50}
+
+    Private Sub frmWallpaper_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'adding items to the comboboxes of length, width and height
+        For i = 0 To 25
+            cboLength.Items.Add(arrayA(i))
+            cboWidth.Items.Add(arrayA(i))
+            cboHeight.Items.Add(arrayA(i))
+        Next
+
+        'adding items to the combobox of roll coverage
+        For j = 0 To 20
+            cboRollCov.Items.Add(arrayB(j))
+        Next
+    End Sub
+
+    Sub Calculate()
+        lstDisplay.Items.Clear() 'delete all the items in the lstDisplay box
+        If (String.IsNullOrWhiteSpace(cboLength.SelectedItem) Or
+            String.IsNullOrWhiteSpace(cboWidth.SelectedItem) Or
+            String.IsNullOrWhiteSpace(cboWidth.SelectedItem) Or
+            String.IsNullOrWhiteSpace(cboRollCov.SelectedItem)) Then
+
+            MsgBox("Invalid! Make all selections.", MsgBoxStyle.Critical)
+        Else
+            intArea = (cboLength.SelectedItem * 2 + cboWidth.SelectedItem * 2) *
+                        cboHeight.SelectedItem 'calculate the area
+            dblRoll = intArea / cboRollCov.SelectedItem 'calculate single roll
+            lstDisplay.Items.Add(Math.Ceiling(dblRoll)) 'display the results
+        End If
+    End Sub
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Calculate() 'when the button is clicked, display the results
+    End Sub
+
+    Private Sub BtnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+
+        'Close the windows form
+        Dim ExitYN As System.Windows.Forms.DialogResult
+        ExitYN = MsgBox("Do you really want to exit?", MsgBoxStyle.YesNo)
+
+        If ExitYN = MsgBoxResult.Yes Then
+            Application.ExitThread()
+        End If
+    End Sub
+
+    Private Sub frmWallpaper_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        'close the form using (x) button
+        Dim dialog As System.Windows.Forms.DialogResult
+        dialog = MsgBox("Do you really want to close?", MsgBoxStyle.YesNo)
+
+        If dialog = MsgBoxResult.No Then
+            e.Cancel = True
+        Else
+            Application.ExitThread()
+        End If
+    End Sub
+End Class
